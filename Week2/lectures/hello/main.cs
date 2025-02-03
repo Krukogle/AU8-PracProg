158fdbd63060b7ba39390d4251007926568a6ed7
Krukogle <im2slick4u@LAPTOP-5KA55333.>
2025-02-03 13:46:30 +0100

    Completed TØ for week 2 (including lecture notes)

---
+++ Week2/lectures/hello/main.cs
@@ -0,0 +1,9 @@
+//using static System.Console;
+//using static System.Math;
+static class main{
+    static int Main(string[] args){
+        System.Console.WriteLine("hello");
+    return 0;        
+    }
+}
+
